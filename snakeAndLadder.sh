8788b1449cd4ace5a3dda2e9e9dbdca46f6862b9
kneeraj825 <kneeraj825@gmail.com>
2020-02-22 10:45:16 +0530

    Added:Checks for a option

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -3,6 +3,26 @@
 echo Welcome To Snake and Ladder Game
 #Constant
 POSITION=0
+WINNING=100
 #checking the random dice number
 diceCheck=$(( 1+ RANDOM%6 ))
+#checking the option
+optionCheck=$(( RANDOM%3 ))
+No_Play=0
+Ladder=1
+Snake=2
+case $optionCheck in
+	$No_Play)
+		POSITION=$POSITION
+		echo "NO_PLAY:$POSITION"
+	;;
+	$Ladder)
+	POSITION=$(($POSITION+$diceCheck))
+		echo "current position: $POSITION"
+	;;
+	$Snake)
+		POSITION=$(($POSITION-$diceCheck))
+		echo "current position after snake bitting: $POSITION"
+	;;
+esac
 
