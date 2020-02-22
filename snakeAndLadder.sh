fc1b7bf26b627cc5aa2d526f967b441cd72f81c7
kneeraj825 <kneeraj825@gmail.com>
2020-02-22 15:25:24 +0530

    Added:Finding the winner player

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,27 +1,68 @@
 #!/bin/bash -x
 
 echo Welcome To Snake and Ladder Game
-#Constant
+function snakeLadder()
+{
+#constants
 POSITION=0
+COUNT=0
 WINNING=100
+PREVPOSITION=0
+#repeating till winning position
+while (( $POSITION != $WINNING ))
+do
 #checking the random dice number
-diceCheck=$(( 1+ RANDOM%6 ))
+	diceCheck=$(( 1+ RANDOM%6 ))
 #checking the option
-optionCheck=$(( RANDOM%3 ))
-No_Play=0
-Ladder=1
-Snake=2
-case $optionCheck in
-	$No_Play)
-		POSITION=$POSITION
-		echo "NO_PLAY:$POSITION"
-	;;
-	$Ladder)
-		POSITION=$(($POSITION+$diceCheck))
-		echo "current position: $POSITION"
-	;;
-	$Snake)
-		POSITION=$(($POSITION-$diceCheck))
-		echo "current position after snake bitting: $POSITION"
-	;;
-esac
+	optionCheck=$(( RANDOM%3 ))
+	No_Play=0
+	Ladder=1
+	Snake=2
+	case $optionCheck in
+		$No_Play)
+			POSITION=$POSITION
+			echo "NO_PLAY:$POSITION"
+		;;
+		$Ladder)
+#checking if position goes beyond to winning position 
+			POSITION=$(($POSITION+$diceCheck))
+			if (($POSITION>WINNING))
+			then
+				POSITION=$PREVPOSITION
+			fi
+         echo "current position: $POSITION"
+      ;;
+      $Snake)
+         POSITION=$(($POSITION-$diceCheck))
+         if (($POSITION<0))
+
+then
+             POSITION=0
+         fi
+         echo "current position after snake bitting: $POSITION"
+         if (($POSITION<WINNING))
+         then
+            PREVPOSITION=$POSITION
+         fi
+      ;;
+   esac
+#counting the total dice roll
+COUNT=$(($COUNT+1))
+echo "Total number of times dice rolled: $COUNT"
+done
+}
+#checking the count of both the players
+snakeLadder
+Player1=$COUNT
+echo "First Player :" $Player1
+snakeLadder
+Player2=$COUNT
+echo "Second Player :" $Player2
+#checking for the winner 
+if (( $Player1 < $Player2 ))
+then
+	echo "First Player is winner:$Player1"
+else
+	echo "Second Player is winner:$Player2"
+fi
+
