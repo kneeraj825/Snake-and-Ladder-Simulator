a45fc273360bea1e5117462579a2f60365f0e6e6
kneeraj825 <kneeraj825@gmail.com>
2020-02-26 16:15:23 +0500

    Refactor:Identation correction

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -3,27 +3,27 @@
 echo Welcome To Snake and Ladder Game
 function snakeLadder()
 {
-#constants
-POSITION=0
-COUNT=0
-WINNING_POSITION=100
-PREV_POSITION=0
-#repeating till winning position
-while (( $POSITION != $WINNING_POSITION ))
-do
-	#checking the random dice number
-	diceCheck=$(( 1+ RANDOM%6 ))
-	#checking the option
-	optionCheck=$(( RANDOM%3 ))
-	No_Play=0
-	LADDER=1
-	SNAKE=2
-	#calling the function
-	choice
-	#counting the total dice roll
-	COUNT=$(($COUNT+1))
-	echo "Total number of times dice rolled: $COUNT"
-done
+	#constants
+	POSITION=0
+	COUNT=0
+	WINNING_POSITION=100
+	PREV_POSITION=0
+	#repeating till winning position
+	while (( $POSITION != $WINNING_POSITION ))
+	do
+		#checking the random dice number
+		diceCheck=$(( 1+ RANDOM%6 ))
+		#checking the option
+		optionCheck=$(( RANDOM%3 ))
+		No_Play=0
+		LADDER=1
+		SNAKE=2
+		#calling the function
+		choice
+		#counting the total dice roll
+		COUNT=$(($COUNT+1))
+		echo "Total number of times dice rolled: $COUNT"
+	done
 }
 function choice()
 {
@@ -57,19 +57,19 @@ function choice()
 }
 function winner()
 {
-#checking the count of both the players
-snakeLadder
-Player1=$COUNT
-echo "First Player :" $Player1
-snakeLadder
-Player2=$COUNT
-echo "Second Player :" $Player2
-#checking for the winner
-if (( $Player1 < $Player2 ))
-then
-	echo "First Player is winner:$Player1"
-else
-	echo "Second Player is winner:$Player2"
-fi
+	#checking the count of both the players
+	snakeLadder
+	Player1=$COUNT
+	echo "First Player :" $Player1
+	snakeLadder
+	Player2=$COUNT
+	echo "Second Player :" $Player2
+	#checking for the winner
+	if (( $Player1 < $Player2 ))
+	then
+		echo "First Player is winner:$Player1"
+	else
+		echo "Second Player is winner:$Player2"
+	fi
 }
 winner
