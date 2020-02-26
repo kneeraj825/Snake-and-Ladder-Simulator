2a39a0f43d3ca0f2fac72b8725011fc530c487cc
kneeraj825 <kneeraj825@gmail.com>
2020-02-26 14:24:46 +0500

    Refactor:Identation and generic correction

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -6,51 +6,57 @@ function snakeLadder()
 #constants
 POSITION=0
 COUNT=0
-WINNING=100
-PREVPOSITION=0
+WINNING_POSITION=100
+PREV_POSITION=0
 #repeating till winning position
-while (( $POSITION != $WINNING ))
+while (( $POSITION != $WINNING_POSITION ))
 do
-#checking the random dice number
+	#checking the random dice number
 	diceCheck=$(( 1+ RANDOM%6 ))
-#checking the option
+	#checking the option
 	optionCheck=$(( RANDOM%3 ))
 	No_Play=0
-	Ladder=1
-	Snake=2
+	LADDER=1
+	SNAKE=2
+	#calling the function
+	choice
+	#counting the total dice roll
+	COUNT=$(($COUNT+1))
+	echo "Total number of times dice rolled: $COUNT"
+done
+}
+function choice()
+{
 	case $optionCheck in
-		$No_Play)
-			POSITION=$POSITION
-			echo "NO_PLAY:$POSITION"
-		;;
-		$Ladder)
-#checking if position goes beyond to winning position 
-			POSITION=$(($POSITION+$diceCheck))
-			if (($POSITION>WINNING))
-			then
-				POSITION=$PREVPOSITION
-			fi
+      $No_Play)
+         POSITION=$POSITION
+         echo "NO_PLAY:$POSITION"
+      ;;
+      $LADDER)
+			#checking if position goes beyond to winning position
+         POSITION=$(($POSITION+$diceCheck))
+         if (($POSITION>WINNING_POSITION))
+         then
+            POSITION=$PREV_POSITION
+         fi
          echo "current position: $POSITION"
       ;;
-      $Snake)
+      $SNAKE)
          POSITION=$(($POSITION-$diceCheck))
          if (($POSITION<0))
-
-then
+			then
              POSITION=0
          fi
          echo "current position after snake bitting: $POSITION"
-         if (($POSITION<WINNING))
+         if (($POSITION<WINNING_POSITION))
          then
-            PREVPOSITION=$POSITION
+            PREV_POSITION=$POSITION
          fi
       ;;
    esac
-#counting the total dice roll
-COUNT=$(($COUNT+1))
-echo "Total number of times dice rolled: $COUNT"
-done
 }
+function winner()
+{
 #checking the count of both the players
 snakeLadder
 Player1=$COUNT
@@ -58,11 +64,12 @@ echo "First Player :" $Player1
 snakeLadder
 Player2=$COUNT
 echo "Second Player :" $Player2
-#checking for the winner 
+#checking for the winner
 if (( $Player1 < $Player2 ))
 then
 	echo "First Player is winner:$Player1"
 else
 	echo "Second Player is winner:$Player2"
 fi
-
+}
+winner
